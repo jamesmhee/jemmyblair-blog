60268c98615169b492a643bed405c0da11b0cf23
jemmyblair <42910234+jamesmhee@users.noreply.github.com>
2024-06-30 01:25:35 +0700

    efiy

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
-
-
 const Loading = ({word, type}:{word:string, type?:string}) => {
   return (
     <div className='w-screen h-screen flex-col bg-zinc-900 flex items-center justify-center z-10 fixed top-0'>
         <span className="loading loading-ring loading-lg"></span>
-        <p>{word}</p>
+        <p>{word}{type}</p>
     </div>
   )
 }
